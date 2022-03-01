6dbb9813486e6ca0c5fff8ca480def5000c26321
Max <max.svedala@gmail.com>
2022-03-01 13:00:06 +0100

    Created Konstruktor and getters/setter in User class

--- src/main/java/client/User.java
+++ src/main/java/client/User.java
@@ -6,9 +6,9 @@ public class User {
     //todo class with username, id, profile pic
     private String username;
     private int id;
-    public User(String username,int id){
+    public User(String username,int id2){
         this.username = username;
-        this.id = id;
+        this.id = id2;
     }
 
     public String getName() {
