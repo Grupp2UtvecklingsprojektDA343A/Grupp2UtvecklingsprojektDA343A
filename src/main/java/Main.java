d35ff0dce5bb0924973fe6d47a5d58b02eaf79e4
Emile <emile@ecspresso.com>
2022-03-02 15:07:36 +0100

    s startar server, sc startar server och client.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,25 @@
 import client.controller.Client;
-import client.model.User;
-
-import java.io.IOException;
 
 public class Main {
     public static void main(String[] args)  {
         if(args.length > 0 && args[0].equals("s")) {
-            // starta server (new Server();)
-        } else {
-            Client client = null;
-            client = new Client();
-            client.showGUI();
+            startServer();
+        } else if(args.length > 0 && args[0].equals("sc")) {
+            startServer();
+            startClient();
         }
+        else {
+            startClient();
+        }
+    }
+
+    private static void startServer() {
+        // starta server (new Server();)
+    }
+
+    private static void startClient() {
+        Client client = null;
+        client = new Client();
+        client.showGUI();
     }
 }
