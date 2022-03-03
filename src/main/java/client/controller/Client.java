c95bb657e6226a3e23f892373930a2d3d5d0b79c
C3dvall <christianedvall@hotmail.com>
2022-03-03 09:30:44 +0100

    Fixade stavfel

--- src/main/java/client/controller/Client.java
+++ src/main/java/client/controller/Client.java
@@ -33,38 +33,30 @@ Servern måste kunna lagra klienter i en objektsamling.
 
 public class Client {
     private MainWindow mainWindow;
-
     private Socket socket;
-
     private User user;
-    private HashMap<String, User> frinendList;
-
+    private HashMap<String, User> friendList;
     private ObjectOutputStream oos;
     private ObjectInputStream ois;
-
     private Message message = null;
 
     public void showGUI() {
         // SwingUtilities.invokeLater(() -> gui = new GUI(this));
         mainWindow = new MainWindow(this);
     }
-
     public void logIn(String username, String host, int port) throws IOException {
         this.user = new User(username, null);
         connect(host, port);
         mainWindow.showContacts(username, null);
     }
-
     public void connect(String ip, int port) throws IOException {
         this.socket = new Socket(ip,port);
         this.oos = new ObjectOutputStream(socket.getOutputStream());
         this.ois = new ObjectInputStream(socket.getInputStream());
     }
-
     public void disconnect() throws IOException {
         socket.close();
     }
-
     public void send(Message message){
         // Skriv en motod som skickar Till servern
         try {
@@ -74,10 +66,8 @@ public class Client {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
-
-    public void recieve(){
+    public void receive(){
         try {
             message = (Message) ois.readObject();
             String sender = String.valueOf(message.getSender());
@@ -108,5 +98,4 @@ public class Client {
             e.printStackTrace();
         }
     }
-
 }
