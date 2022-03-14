a6595cc1146663dbd72dcf5281306df44185867b
C3dvall <christianedvall@hotmail.com>
2022-03-14 13:29:39 +0100

    Skapar ett messageobjekt innehållandes en vänlista och returnerar denna. Sedan läses denna in och skickas med i uppstart av klient.

--- src/main/java/server/entity/Server.java
+++ src/main/java/server/entity/Server.java
@@ -1,11 +1,14 @@
 package server.entity;
+
 import client.control.Client;
 import globalEntity.Message;
 import globalEntity.User;
 import server.control.Controller;
+
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -37,7 +40,7 @@ public class Server implements PropertyChangeListener {
         new Connection().start();
     }
 
-    public void addListener(PropertyChangeListener listener){
+    public void addListener(PropertyChangeListener listener) {
         pcs.addPropertyChangeListener(listener);
     }
 
@@ -56,12 +59,12 @@ public class Server implements PropertyChangeListener {
 
     public void createFriendList(User user, ArrayList<User> users) {
         ArrayList<User> friends = new ArrayList<>(users.size());
-        for(int i = 0; i < friends.size(); i++) {
+        for (int i = 0; i < friends.size(); i++) {
             friends.add(users.get(i));
         }
-        String filename = String.format("files/"+user.getUsername()+"_friends.dat");
+        String filename = String.format("files/" + user.getUsername() + "_friends.dat");
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
-            for(User friend : friends) {
+            for (User friend : friends) {
                 oos.writeObject(friend);
             }
             oos.flush();
@@ -73,7 +76,7 @@ public class Server implements PropertyChangeListener {
     public synchronized void messagesToSend(Message message) {
         User receiver = message.getReceiver();
         Buffer<Message> buffer;
-        if(messageOnHold.containsKey(receiver)) {
+        if (messageOnHold.containsKey(receiver)) {
             buffer = messageOnHold.get(receiver);
             buffer.put(message);
         } else {
@@ -87,23 +90,32 @@ public class Server implements PropertyChangeListener {
     public void propertyChange(PropertyChangeEvent evt) {
 
     }
-    public ArrayList<User> readFriendList(User user) {
-        String filename = String.format("files/"+user.getUsername()+"_friends.dat");
+
+    public Message readFriendList(User user) {
+        String filename = String.format("files/" + user.getUsername() + "_friends.dat");
         ArrayList<User> friends = new ArrayList<>();
+        Message message = null;
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
-            for(User friend : friends) {
+            for (User friend : friends) {
                 friend = (User) ois.readObject();
                 friends.add(friend);
             }
+            message = new Message.Builder()
+                .type(Message.CONTACTS)
+                .contacts(friends)
+                .receiver(user)
+                .build();
+
+            //Lägg in arraylisten som ett message-objekt, ska gå att göra eftersom vi har builder.
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
-        return friends;
+        return message; //Här ska det returneras ett messageobjekt istället.
     }
 
     public void sendMessage(Message reply) {
         User receiver = reply.getReceiver();
-        if(loggedInUsers.containsKey(receiver)) {
+        if (loggedInUsers.containsKey(receiver)) {
             ClientHandler clientHandler = loggedInUsers.get(receiver);
             clientHandler.send(reply);
         } else {
@@ -113,7 +125,7 @@ public class Server implements PropertyChangeListener {
 
     private void updateListForAllContacts() {
         synchronized (loggedInUsers) {
-            for(Map.Entry<User, ClientHandler> entry : loggedInUsers.entrySet()) {
+            for (Map.Entry<User, ClientHandler> entry : loggedInUsers.entrySet()) {
 
                 Message message = new Message.Builder()
                     .type(Message.CONTACTS)
@@ -125,8 +137,8 @@ public class Server implements PropertyChangeListener {
     }
 
     public boolean userExists(User user) {
-            return loggedInUsers.containsKey(user);
-        }
+        return loggedInUsers.containsKey(user);
+    }
 
     private class Connection extends Thread {
 
@@ -143,7 +155,7 @@ public class Server implements PropertyChangeListener {
 
                     Message msg = (Message) ois.readObject();
 
-                    if(msg.getType() == Message.LOGIN) {
+                    if (msg.getType() == Message.LOGIN) {
                         new LoginHandler(socket, oos, ois, msg.getSender()).start();
                         pcs.firePropertyChange("login", null, msg.getSender().getUsername());
                         controller.addToTraffic(msg.getSender().getUsername());
@@ -172,27 +184,35 @@ public class Server implements PropertyChangeListener {
             this.ois = ois;
             this.user = user;
         }
+
         @Override
         public void run() {
             ClientHandler clientHandler = null;
             try {
-                while(clientHandler == null) {
-                    if(user == null) {
+                while (clientHandler == null) {
+                    if (user == null) {
                         Message message = (Message) ois.readObject();
                         user = message.getSender();
                     }
                     Message reply;
 
-                    if(!controller.userExists(user)) { // kan logga in
-                        reply =  new Message.Builder().type(Message.LOGIN_SUCCESS).build();
+                    if (! controller.userExists(user)) { // kan logga in
+                        reply = new Message.Builder().type(Message.LOGIN_SUCCESS).build();
                         clientHandler = new ClientHandler(controller, socket, oos, ois);
                         addLoggedInUser(user, clientHandler);
                         //client.addPropertyChangeListener((PropertyChangeListener) this);
                         clientHandler.start();
+                        readFriendList(user);
                         clientHandler.getServerSender().send(reply);
-                        if(messageOnHold.containsKey(user)) {
+                        File file = new File("files/" + user.getUsername() + "_friends.dat");
+                        if (file.exists()) {
+                            clientHandler.getServerSender().send(readFriendList(user));
+                        } else {
+                            System.out.println("No friend list in directory.");
+                        }
+                        if (messageOnHold.containsKey(user)) {
                             Buffer buffer = messageOnHold.get(user);
-                            while(!buffer.isEmpty()) {
+                            while (! buffer.isEmpty()) {
                                 try {
                                     clientHandler.getServerSender().send((Message) buffer.get());
                                 } catch (InterruptedException e) {
@@ -201,7 +221,7 @@ public class Server implements PropertyChangeListener {
                             }
                         }
                     } else { // kan inte logga in
-                        reply =  new Message.Builder().type(Message.LOGIN_FAILED).build();
+                        reply = new Message.Builder().type(Message.LOGIN_FAILED).build();
                         oos.writeObject(reply);
                         oos.flush();
                         user = null;
@@ -212,27 +232,27 @@ public class Server implements PropertyChangeListener {
             }
             interrupt(); // Stoppa tråden
         }
-        public void closeThread(PropertyChangeEvent evt){
-            if(evt.getPropertyName().equals("true") && evt.getNewValue() instanceof User){
-                if(loggedInUsers.containsKey(evt.getNewValue())){
+
+        public void closeThread(PropertyChangeEvent evt) {
+            if (evt.getPropertyName().equals("true") && evt.getNewValue() instanceof User) {
+                if (loggedInUsers.containsKey(evt.getNewValue())) {
                     loggedInUsers.get(evt.getNewValue()).closeThread();
                     //loggedInUsers.get(evt.getNewValue()).interrupt();
-                }
-                else {
+                } else {
                     System.out.println("User not found");
                 }
             }
         }
     }
-    public void disconnect(Message message){
+
+    public void disconnect(Message message) {
         User user = message.getSender();
-        if(loggedInUsers.containsKey(user)){
+        if (loggedInUsers.containsKey(user)) {
             loggedInUsers.get(user).closeThread();
             loggedInUsers.remove(user);
             //loggedInUsers.get(evt.getNewValue()).interrupt();
             updateListForAllContacts();
-        }
-        else {
+        } else {
             System.out.println("User not found");
         }
 
